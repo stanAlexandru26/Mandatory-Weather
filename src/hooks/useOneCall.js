0c315915f3c510677bef123808c33b41081c3cc2
Stan Alexandru-Stefan <stan.alexandru26@gmail.com>
2022-03-19 00:42:29 +0200

    fixed error that ran useOneCall when lat/long was undefined

--- src/hooks/useOneCall.js
+++ src/hooks/useOneCall.js
@@ -19,87 +19,93 @@ export default function useOneCall(position, units) {
   const oneCall_URL = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=${units}&exclude=minutely&appid=${API_KEY}`;
 
   const [oneCall, setOneCall] = useState({});
+
   useEffect(() => {
-    axios.get(oneCall_URL).then((response) => {
-      setOneCall({
-        current: {
-          city: cityName,
-          country: countryNameinEnglish.of(cityCountry),
-          timezone: response.data.timezone,
-          time: response.data.timezone_offset + response.data.current.dt,
-
-          temp: Math.round(response.data.current.temp),
-          feels_like: Math.round(response.data.current.feels_like),
-          humidity: response.data.current.humidity,
-          wind_speed: response.data.current.wind_speed,
-          wind_deg: response.data.current.wind_deg,
-          weather_id: response.data.current.weather[0].id,
-          weather_main: response.data.current.weather[0].main,
-          weather_description: capitalize(
-            response.data.current.weather[0].description,
-          ),
-          weather_icon: response.data.current.weather[0].icon,
-          sunSet: response.data.timezone_offset + response.data.current.sunset,
-          sunRise:
-            response.data.timezone_offset + response.data.current.sunrise,
-        },
-        hourly: response.data.hourly.map((hour) => {
-          return {
-            time: hour.dt,
-            temp: Math.round(hour.temp),
-            weather_id: hour.weather[0].id,
-            weather_main: hour.weather[0].main,
-            weather_description: capitalize(hour.weather[0].description),
-            weather_icon: hour.weather[0].icon,
-            wind_speed: Math.round(hour.wind_speed * 3.6),
-            wind_deg: hour.wind_deg,
-          };
-        }),
-        daily: response.data.daily.map((day) => {
-          return {
-            time: day.dt,
-            temp_min: Math.round(day.temp.min),
-            temp_max: Math.round(day.temp.max),
-            weather_id: day.weather[0].id,
-            weather_main: day.weather[0].main,
-            weather_description: capitalize(day.weather[0].description),
-            weather_icon: day.weather[0].icon,
-            wind_speed: day.wind_speed,
-            wind_deg: day.wind_deg,
-            sunSet: day.sunset,
-            sunRise: day.sunrise,
-          };
-        }),
-
-        chartData: {
-          hourlyChartData: response.data.hourly.map((hour) => {
+    async function fetchData() {
+      await axios.get(oneCall_URL).then((response) => {
+        setOneCall({
+          current: {
+            city: cityName,
+            country: countryNameinEnglish.of(cityCountry),
+            timezone: response.data.timezone,
+            time: response.data.timezone_offset + response.data.current.dt,
+            temp: Math.round(response.data.current.temp),
+            feels_like: Math.round(response.data.current.feels_like),
+            humidity: response.data.current.humidity,
+            wind_speed: response.data.current.wind_speed,
+            wind_deg: response.data.current.wind_deg,
+            weather_id: response.data.current.weather[0].id,
+            weather_main: response.data.current.weather[0].main,
+            weather_description: capitalize(
+              response.data.current.weather[0].description,
+            ),
+            weather_icon: response.data.current.weather[0].icon,
+            sunSet:
+              response.data.timezone_offset + response.data.current.sunset,
+            sunRise:
+              response.data.timezone_offset + response.data.current.sunrise,
+          },
+          hourly: response.data.hourly.map((hour) => {
             return {
-              xAxis: moment.unix(hour.dt).utc().format('dddd '),
               time: hour.dt,
-              weather_description: capitalize(hour.weather[0].description),
               temp: Math.round(hour.temp),
-              weather_icon: hour.weather[0].icon,
               weather_id: hour.weather[0].id,
+              weather_main: hour.weather[0].main,
+              weather_description: capitalize(hour.weather[0].description),
+              weather_icon: hour.weather[0].icon,
+              wind_speed: Math.round(hour.wind_speed * 3.6),
+              wind_deg: hour.wind_deg,
             };
           }),
-          dailyChartData: response.data.daily.map((day) => {
+          daily: response.data.daily.map((day) => {
             return {
-              xAxis: moment.unix(day.dt).utc().format('dddd'),
               time: day.dt,
               temp_min: Math.round(day.temp.min),
               temp_max: Math.round(day.temp.max),
-              temp: Math.round(day.temp.day),
-              rainProbability: (day.pop * 100).toFixed(0),
+              weather_id: day.weather[0].id,
+              weather_main: day.weather[0].main,
               weather_description: capitalize(day.weather[0].description),
               weather_icon: day.weather[0].icon,
-              weather_id: day.weather[0].id,
+              wind_speed: day.wind_speed,
+              wind_deg: day.wind_deg,
               sunSet: day.sunset,
               sunRise: day.sunrise,
             };
           }),
-        },
+
+          chartData: {
+            hourlyChartData: response.data.hourly.map((hour) => {
+              return {
+                xAxis: moment.unix(hour.dt).utc().format('dddd '),
+                time: hour.dt,
+                weather_description: capitalize(hour.weather[0].description),
+                temp: Math.round(hour.temp),
+                weather_icon: hour.weather[0].icon,
+                weather_id: hour.weather[0].id,
+              };
+            }),
+            dailyChartData: response.data.daily.map((day) => {
+              return {
+                xAxis: moment.unix(day.dt).utc().format('dddd'),
+                time: day.dt,
+                temp_min: Math.round(day.temp.min),
+                temp_max: Math.round(day.temp.max),
+                temp: Math.round(day.temp.day),
+                rainProbability: (day.pop * 100).toFixed(0),
+                weather_description: capitalize(day.weather[0].description),
+                weather_icon: day.weather[0].icon,
+                weather_id: day.weather[0].id,
+                sunSet: day.sunset,
+                sunRise: day.sunrise,
+              };
+            }),
+          },
+        });
       });
-    });
+    }
+    if (cityName && cityCountry) {
+      fetchData();
+    }
   }, [cityName]);
 
   return oneCall;
